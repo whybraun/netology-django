99871f28f0ddff216fe5fa768fba8878e2bf15cf
Dimitriy Murzin <dimitriymurzin@gmail.com>
2023-11-07 20:54:51 +0400

    add: homework recipes 07/11/2023

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,30 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def omlet(request):
+    res = {}
+    servings = request.GET.get("servings", 1)
+    for i, e in DATA['omlet'].items():
+        res.update({i: e * int(servings)})
+    context = {'recipe': res}
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    res = {}
+    servings = request.GET.get("servings", 1)
+    for i, e in DATA['pasta'].items():
+        res.update({i: e * int(servings)})
+    context = {'recipe': res}
+    return render(request, 'calculator/index.html', context)
+
+
+def buter(request):
+    res = {}
+    servings = request.GET.get("servings", 1)
+    for i, e in DATA['buter'].items():
+        res.update({i: e * int(servings)})
+    context = {'recipe': res}
+    return render(request, 'calculator/index.html', context)
