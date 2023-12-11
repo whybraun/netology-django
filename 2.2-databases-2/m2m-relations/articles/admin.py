07c580a02a91fb3937fa6d5c221822646977eb9d
Dimitriy Murzin <dimitriymurzin@gmail.com>
2023-12-11 11:06:08 +0400

    update 11/12/2023

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,54 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import forms, BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, ArticleTag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        tags_list = []
+        for form in self.forms:
+            if form.cleaned_data:
+                if form.cleaned_data["is_main"]:
+                    count += 1
+                if "tag_name" in form.cleaned_data:
+                    if form.cleaned_data["tag_name"] not in tags_list:
+                        tags_list.append(form.cleaned_data["tag_name"])
+                    else:
+                        raise ValidationError(
+                            f"Тег {form.cleaned_data['tag_name']} указан больше одного раза !!!"
+                        )
+            else:
+                print("empty dict() !!!!!!!!")
+        if count == 0:
+            raise ValidationError("Основной раздел не указан!")
+        if count >= 2:
+            raise ValidationError("Основной раздел может быть один!")
+        return super().clean()
+
+
+class ArticleTagInline(admin.TabularInline):
+    model = ArticleTag
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "id",
+        "title",
+        "text",
+        "published_at",
+        "image",
+    ]
+    inlines = [
+        ArticleTagInline,
+    ]
+    list_filter = ["published_at"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "subject"]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,15 +2,38 @@ from django.db import models
 
 
 class Article(models.Model):
-
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    title = models.CharField(max_length=256, verbose_name="Название")
+    text = models.TextField(verbose_name="Текст")
+    published_at = models.DateTimeField(verbose_name="Дата публикации")
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        verbose_name="Изображение",
+    )
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    subject = models.CharField(max_length=200, verbose_name="name")
+    article = models.ManyToManyField(Article, related_name="tag", through="ArticleTag")
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = "Раздел"
+        verbose_name_plural = "Разделы"
+
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="scopes"
+    )
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name="scopes")
+    is_main = models.BooleanField(default=False)
